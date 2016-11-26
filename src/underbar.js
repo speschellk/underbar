ccee66fb9dfde11e1a78b16490af366dc56ba354
Kay Christensen <kaychristensen@gmail.com>
2016-11-25 23:28:45 -0800

    "each" and "indexOf" tests pass

--- src/underbar.js
+++ src/underbar.js
@@ -48,10 +48,17 @@
   //
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
+
   _.each = function(collection, iterator) {
-    for (var key in collection) {
-      iterator(collection[key], key, collection);
-    }
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      };
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      };
+    };
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -65,9 +72,8 @@
     _.each(array, function(item, index) {
       if (item === target && result === -1) {
         result = index;
-      }
+      };
     });
-
     return result;
   };
 
