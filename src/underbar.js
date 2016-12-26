829fdb48f39ddce1b343fe0d426b3fc4da414b9f
Kay Christensen <kaychristensen@gmail.com>
2016-12-26 12:38:30 -0800

    completed '_.first' and '_.last' methods

--- src/underbar.js
+++ src/underbar.js
@@ -32,12 +32,13 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
-    
+    return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length - n));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
